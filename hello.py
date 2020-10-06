35d75724b1688c0cf31408b07fc8a6c8e624ca12
shaun-adam <shaun.storbakken@gmail.com>
2020-10-05 21:57:37 -0600

    updated hello

--- hello.py
+++ hello.py
@@ -1,3 +1,15 @@
-msg = "Hello World"
-print(msg)
-print("hey there")
+import subprocess
+import time
+
+def islocked():
+    time.sleep(5)
+    process_name='LogonUI.exe'
+    callall='TASKLIST'
+    outputall=subprocess.check_output(callall)
+    outputstringall=str(outputall)
+    if process_name in outputstringall:
+        print("Locked.")
+    else: 
+        print("Unlocked.")
+    
+islocked()
